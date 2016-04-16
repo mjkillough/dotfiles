2d63b050e11e0ac409d4853d37dd21cb833373ae
Michael Killough <michaeljkillough@gmail.com>
2016-04-17 00:05:57 +0100

    Backup dotfiles.

---
+++ .config/qtile/config.py
@@ -0,0 +1,129 @@
+from libqtile.config import Key, Screen, Group, Drag, Click
+from libqtile.command import lazy
+from libqtile import layout, bar, widget
+
+mod = "mod4"
+
+keys = [
+    Key(
+        [], 'XF86MonBrightnessUp',
+        lazy.spawn("xbacklight -inc 10")
+    ),
+    Key(
+        [], 'XF86MonBrightnessDown',
+        lazy.spawn("xbacklight -dec 10")
+    ),
+    Key([], 'XF86AudioRaiseVolume', lazy.spawn('amixer -q set Master 5%+')),
+    Key([], 'XF86AudioLowerVolume', lazy.spawn('amixer -q set Master 5%-')),
+    Key([], 'XF86AudioMute', lazy.spawn('amixer -q set Master toggle')),
+
+    # Switch between windows in current stack pane
+    Key(
+        [mod], "k",
+        lazy.layout.down()
+    ),
+    Key(
+        [mod], "j",
+        lazy.layout.up()
+    ),
+
+    # Move windows up or down in current stack
+    Key(
+        [mod, "control"], "k",
+        lazy.layout.shuffle_down()
+    ),
+    Key(
+        [mod, "control"], "j",
+        lazy.layout.shuffle_up()
+    ),
+
+    # Switch window focus to other pane(s) of stack
+    Key(
+        [mod], "space",
+        lazy.layout.next()
+    ),
+
+    # Swap panes of split stack
+    Key(
+        [mod, "shift"], "space",
+        lazy.layout.rotate()
+    ),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"], "Return",
+        lazy.layout.toggle_split()
+    ),
+    Key([mod], "Return", lazy.spawn("urxvt")),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout()),
+    Key([mod], "w", lazy.window.kill()),
+
+    Key([mod, "control"], "r", lazy.restart()),
+    Key([mod, "control"], "q", lazy.shutdown()),
+    Key([mod], "r", lazy.spawn("lighthouse-launcher")),
+    Key([mod], "e", lazy.spawncmd()),
+]
+
+groups = [Group(i) for i in "asdf"]
+
+for i in groups:
+    # mod1 + letter of group = switch to group
+    keys.append(
+        Key([mod], i.name, lazy.group[i.name].toscreen())
+    )
+
+    # mod1 + shift + letter of group = switch to & move focused window to group
+    keys.append(
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name))
+    )
+
+layouts = [
+    layout.Max(),
+    layout.Stack(num_stacks=2)
+]
+
+widget_defaults = dict(
+    font='SourceCodePro',
+    fontsize=28,
+    padding=8
+)
+
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.GroupBox(highlight_method='block', padding=0, padding_x=8, margin=0,borderwidth=0),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.Systray(),
+                widget.Volume(),
+                widget.Battery(format='({percent:2.0%} - {hour:d}:{min:02d})'),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+            ],
+            34,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+        start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+        start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating()
+auto_fullscreen = True
